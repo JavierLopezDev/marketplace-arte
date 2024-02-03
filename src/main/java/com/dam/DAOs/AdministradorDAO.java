384ffa1153961b304e51b5056d63627d31ec099c
Javier L <javierlopezandujar2003@gmail.com>
2024-02-03 18:43:37 +0100

    Add the first version of the DAO and its administrator implementation

---
+++ src/main/java/com/dam/DAOs/AdministradorDAO.java
@@ -0,0 +1,11 @@
+package com.dam.DAOs;
+
+import com.dam.entidades.Administrador;
+
+public interface AdministradorDAO {
+
+    public boolean insertarAdministrador(Administrador administrador);
+    public boolean editarAdministrador(Administrador administrador);
+    public boolean borrarAdministrador(Administrador administrador);
+    public Administrador obtenerAdministrador(int idAdministrador);
+}
---
+++ src/main/java/com/dam/implementacionesDAO/AdministradorDAOImpl.java
@@ -0,0 +1,64 @@
+package com.dam.implementacionesDAO;
+
+import com.dam.DAOs.AdministradorDAO;
+import com.dam.entidades.Administrador;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class AdministradorDAOImpl implements AdministradorDAO {
+
+    EntityManagerFactory emf;
+    EntityManager em;
+    String up = "upArte";
+    public AdministradorDAOImpl() {
+        emf = Persistence.createEntityManagerFactory(up);
+    }
+    @Override
+    public boolean insertarAdministrador(Administrador administrador) {
+        em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(administrador);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean editarAdministrador(Administrador administrador) {
+        em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(administrador);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean borrarAdministrador(Administrador administrador) {
+        em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.remove(administrador);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return false;
+        }
+    }
+
+    @Override
+    public Administrador obtenerAdministrador(int idAdministrador) {
+        em = emf.createEntityManager();
+        return em.find(Administrador.class, idAdministrador);
+    }
+}
